6524e150d417e291ac1f233f65a4930f8d5a619c
artemtalmenev <artemtalmenev@mail.ru>
2020-04-04 00:12:27 +0300

    Подключение Firebase login

--- src/main.js
+++ src/main.js
@@ -8,14 +8,37 @@ import messagePlugin from '@/utils/message.plugin'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.config.productionTip = false
 
 Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp({
+  apiKey: "AIzaSyDOx90R21GBie-fTL4BrvxTBk1t2ItCkKo",
+  authDomain: "vue-diplom-crm.firebaseapp.com",
+  databaseURL: "https://vue-diplom-crm.firebaseio.com",
+  projectId: "vue-diplom-crm",
+  storageBucket: "vue-diplom-crm.appspot.com",
+  messagingSenderId: "968181609113",
+  appId: "1:968181609113:web:bb7c9eb5824b75a324be08",
+  measurementId: "G-NB1DQ4MBGT"
+})
+
+let app 
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
+
+
