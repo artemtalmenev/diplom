4163f194d2f2ae564355ef363a8054b2df920dac
artemtalmenev <artemtalmenev@mail.ru>
2020-04-04 02:42:58 +0300

    Подключение регистрации Firebase

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,3 +1,6 @@
 export default {
-  'logout': 'Вы вышли из системы'
+  'logout': 'Вы вышли из системы',
+  'auth/user-not-found': 'Пользователя с таким email не существует',
+  'auth/wrong-password': 'Пароль введен неверно',
+  'auth/email-already-in-use': 'Такой email уже занят'
 }
